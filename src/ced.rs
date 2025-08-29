dbd0bcb320148c85e68b263d170e66a16009eb02
raanyiel <cgunnar05@gmail.com>
2025-08-29 17:39:52 -0500

    perf: Removed replaces in ced()

--- src/ced.rs
+++ src/ced.rs
@@ -2,16 +2,20 @@ use crate::Command;
 use crate::USAGE;
 use lib_base64::Base64;
 
-pub fn ced<T: AsRef<str>>(cmd: &Command, msg: T) {
+pub fn ced(cmd: &Command, msg: impl AsRef<str>) {
     if msg.as_ref().is_empty() {
         eprintln!("{}", USAGE);
         return;
     }
-
-    let msg = msg
-        .as_ref()
-        .replace("encoded text: ", "")
-        .replace("decoded text: ", "");
+    let msg = msg.as_ref();
+    let msg = if let Some(x) = msg.strip_prefix("encoded text: ") {
+        x
+    } else if let Some(x) = msg.strip_prefix("decoded text: ") {
+        x
+    } else {
+        msg
+    }
+    .to_string();
 
     match cmd {
         Command::Encode => match msg.encode() {
